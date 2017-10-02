110470539623855f940de2ccb14258e0868538c6
sonaidgr8 <mitra_anasua@yahoo.in>
2017-10-02 22:53:21 +0530

    35

--- main_algo.py
+++ main_algo.py
@@ -24,7 +24,7 @@ def get_ArgumentParser():
     parser = argparse.ArgumentParser()
 
     # Experiment configuration
-    parser.add_argument("--DATA_DIR", default="cora")
+    parser.add_argument("--DATA_DIR", default="cornell")
     parser.add_argument("--LOG_DIR", default="emb/")
     parser.add_argument("--FOLDER_SUFFIX", default=" ")
     parser.add_argument("--LOG_FNAME", default="mod_mnf.log")
@@ -36,10 +36,10 @@ def get_ArgumentParser():
 
     # Weights for different components in the objective functions
     parser.add_argument("--ETA", default=1.0, help="First and Second order proximity mixing parameters")
-    parser.add_argument("--ALPHA", default=10.0, help="Similarity matrix factorization weight")
-    parser.add_argument("--BETA", default=0.7, help="Community Indicator matrix factorization weight")
+    parser.add_argument("--ALPHA", default=0.5, help="Similarity matrix factorization weight")
+    parser.add_argument("--BETA", default=0.1, help="Community Indicator matrix factorization weight")
     parser.add_argument("--THETA", default=1.0, help="Label matrix factorization weight")
-    parser.add_argument("--GAMMA", default=1.0, help="Modularity Maximization weight")
+    parser.add_argument("--GAMMA", default=3.0, help="Modularity Maximization weight")
     parser.add_argument("--PHI", default=1.0, help="Network regularization weight")
     parser.add_argument("--DELTA", default=1.0, help="Simple Network regularization weight")
     parser.add_argument("--LAMBDA", default=1.0, help="L2 regularization weight for S and H factorization")
@@ -47,9 +47,9 @@ def get_ArgumentParser():
     # paper to code attribute mapping : alpha for S factorization, beta for H factorization, gamma for modularity maximization
 
     # Factorization parameters
-    parser.add_argument("--MAX_ITER", default=50)
+    parser.add_argument("--MAX_ITER", default=500)
     parser.add_argument("--L_COMPONENTS", default=128)
-    parser.add_argument("--K", default=5)
+    parser.add_argument("--K", default=3)
     parser.add_argument("--INIT", default="random")
     parser.add_argument("--PROJ", default=True)
     parser.add_argument("--COST_F", default='LS')
